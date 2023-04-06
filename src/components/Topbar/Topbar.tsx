c8e692c620b506d907b06eec8561d3cdc2476d0d
noblu <nguyenkiencuong09c4a@gmail.com>
2023-04-07 00:42:46 +0700

    update

--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -3,13 +3,13 @@ import { HiOutlineMail, MdLocationPin, BsTelephoneFill } from "react-icons/all";
 const Topbar = () => {
   return (
     <div className="h-12 bg-pink flex items-center justify-center">
-      <span className="flex items-center text-[17px] md:text-xl mr-4">
+      <span className="flex items-center text-sm sm:text-[17px] md:text-xl mr-4">
         <MdLocationPin /> 600 TÔN ĐỨC THẮNG
       </span>
       <span className="hidden lg:flex items-center text-xl px-10">
         <HiOutlineMail className="mr-1" /> OXDoor.VN@gmail.com
       </span>
-      <span className="flex items-center text-[17px] md:text-xl">
+      <span className="flex items-center text-[16px] sm:text-[17px] md:text-xl">
         <BsTelephoneFill className="mr-1" /> HOTLINE : 0905695856 - 0975317123
       </span>
     </div>
