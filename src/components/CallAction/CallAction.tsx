563a192b6785ee50790fe0ca10050d989d7f5dc1
noblu <nguyenkiencuong09c4a@gmail.com>
2023-04-07 18:46:07 +0700

    update

--- src/components/CallAction/CallAction.tsx
+++ src/components/CallAction/CallAction.tsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import { BsFillTelephoneOutboundFill } from 'react-icons/bs';
+import React from "react";
+import { BsFillTelephoneOutboundFill } from "react-icons/bs";
 
 const CallAction = () => {
   return (
-    <div className="absolute left-4 bottom-[10px] translate-[-50%]">
+    <div className="absolute right-4 bottom-[10px] translate-[-50%]">
       <div className="ping">
         <BsFillTelephoneOutboundFill className=" text-pink rounded-full h-5 w-5 ml-[-5px] mr-2" />
-        0999999999999
+        0905695856
       </div>
     </div>
   );
-}
+};
 
-export default CallAction
+export default CallAction;
